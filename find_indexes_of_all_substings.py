946154c2835f493ad357af4d92abd50e13a22f9e
mars1198 <emperie2006@yandex.ru>
2018-05-27 17:53:42 +0400

    Create find_indexes_of_all_substings.py

---
+++ find_indexes_of_all_substings.py
@@ -0,0 +1,12 @@
+def find_all(a_string, sub):
+    result = []
+    k = 0
+    while k < len(a_string):
+        k = a_string.find(sub, k)
+        if k == -1:
+            return result
+        else:
+            result.append(k)
+            k += 1 #change to k += len(sub) to not search overlapping results
+    return result
+    
