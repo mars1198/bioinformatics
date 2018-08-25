eed2f8df49e52d8a96e06b88c579759136bcb1f3
mars1198 <emperie2006@yandex.ru>
2018-08-25 17:18:14 +0300

    Create WeightedDie.py

---
+++ WeightedDie.py
@@ -0,0 +1,9 @@
+import random
+# Input:  A dictionary Probabilities whose keys are k-mers and whose values are the probabilities of these kmers
+# Output: A randomly chosen k-mer with respect to the values in Probabilities
+def WeightedDie(Probabilities):
+    n = random.uniform(0, 1)
+    for p in Probabilities:
+        n -= Probabilities[p]
+        if n <= 0:
+            return p
