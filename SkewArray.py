4d4dbdc165397814babb6abae2b8a8d0f44a9922
mars1198 <emperie2006@yandex.ru>
2018-08-25 13:13:16 +0300

    Create SkewArray.py

---
+++ SkewArray.py
@@ -0,0 +1,8 @@
+def SkewArray(Genome):
+    skew = [0]
+    score = {"A":0, "T":0, "C":-1, "G":1}
+    for i in range(1,len(Genome)+1):
+            skew.append(score[Genome[i-1]] + skew[i-1])
+    return skew
+
+
