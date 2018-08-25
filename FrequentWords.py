7b3287e48a4d8bd7eb407dbbfa75a8368445761d
mars1198 <emperie2006@yandex.ru>
2018-08-25 11:31:42 +0300

    Create FrequentWords.py

---
+++ FrequentWords.py
@@ -0,0 +1,48 @@
+def FrequentWords(Text, k):
+    FrequentPatterns = []
+    Count = CountDict(Text, k)
+    m = max(Count.values())
+    for i in Count:
+        if Count[i] == m:
+            FrequentPatterns.append(Text[i:i+k])
+    FrequentPatternsNoDuplicates = remove_duplicates(FrequentPatterns)
+    return FrequentPatternsNoDuplicates
+
+
+# Input:  A list Items
+# Output: A list containing all objects from Items without duplicates
+def remove_duplicates(Items):
+    ItemsNoDuplicates = [] # output variable
+    for i in Items:
+        if i not in ItemsNoDuplicates:
+            ItemsNoDuplicates.append(i)
+    return ItemsNoDuplicates
+
+# Input:  A string Text and an integer k
+# Output: CountDict(Text, k)
+# HINT:   This code should be identical to when you last implemented CountDict
+def CountDict(Text, k):
+    Count = {}
+    for i in range(len(Text)-k+1):
+        Pattern = Text[i:i+k]
+        Count[i] = PatternCount(Pattern, Text)
+    return Count
+
+# Input:  Strings Pattern and Text
+# Output: The number of times Pattern appears in Text
+# HINT:   This code should be identical to when you last implemented PatternCount
+def PatternCount(Pattern, Text):
+    count = 0
+    for i in range(len(Text)-len(Pattern)+1):
+        if Text[i:i+len(Pattern)] == Pattern:
+            count = count+1
+    return count
+
+# Now set Text equal to the Vibrio cholerae oriC and k equal to 10
+Text = 'ATCAATGATCAACGTAAGCTTCTAAGCATGATCAAGGTGCTCACACAGTTTATCCACAACCTGAGTGGATGACATCAAGATAGGTCGTTGTATCTCCTTCCTCTCGTACTCTCATGACCACGGAAAGATGATCAAGAGAGGATGATTTCTTGGCCATATCGCAATGAATACTTGTGACTTGTGCTTCCAATTGACATCTTCAGCGCCATATTGCGCTGGCCAAGGTGACGGAGCGGGATTACGAAAGCATGATCATGGCTGTTGTTCTGTTTATCTTGTTTTGACTGAGACTTGTTAGGATAGACGGTTTTTCATCACTGACTAGCCAAAGCCTTACTCTGCCTGACATCGACCGTAAATTGATAATGAATTTACATGCTTCCGCGACGATTTACCTCTTGATCATCGATCCGATTGAAGATCTTCAATTGTTAATTCTCTTGCCTCGACTCATAGCCATGATGAGCTCTTGATCATGTTTCCTTAACCCTCTATTTTTTACGGAAGAATGATCAAGCTGCTGCTCTTGATCATCGTTTC'
+
+k = 10
+
+# Finally, print the result of calling FrequentWords on Text and k.
+
+print (FrequentWords(Text, k))
