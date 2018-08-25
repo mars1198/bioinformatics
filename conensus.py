14d0da5baef93c029eb7d9e8e86d7234f15a2f4d
mars1198 <emperie2006@yandex.ru>
2018-08-25 14:28:43 +0300

    Create conensus.py

---
+++ conensus.py
@@ -0,0 +1,30 @@
+# Insert your Count(Motifs) function here.
+def Count(Motifs):
+    count = {} # initializing the count dictionary
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(0)
+
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    return count
+# Input:  A set of kmers Motifs
+# Output: A consensus string of Motifs.
+def Consensus(Motifs):
+    k = len(Motifs[0])
+    count = Count(Motifs)
+    consensus = ""
+    for j in range(k):
+        m = 0
+        frequentSymbol = ""
+        for symbol in "ACGT":
+            if count[symbol][j] > m:
+                m = count[symbol][j]
+                frequentSymbol = symbol
+        consensus += frequentSymbol
+    return consensus
