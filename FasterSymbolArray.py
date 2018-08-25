9fcf2634314f70de1d41b549736f5a486215556c
mars1198 <emperie2006@yandex.ru>
2018-08-25 12:57:12 +0300

    Create FasterSymbolArray.py

---
+++ FasterSymbolArray.py
@@ -0,0 +1,36 @@
+# Input:  Strings Genome and symbol
+# Output: FasterSymbolArray(Genome, symbol)
+def FasterSymbolArray(Genome, symbol):
+    array = {}
+    n = len(Genome)
+    ExtendedGenome = Genome + Genome[0:n//2]
+
+    # look at the first half of Genome to compute first array value
+    array[0] = PatternCount(symbol, Genome[0:n//2])
+
+    for i in range(1, n):
+        # start by setting the current array value equal to the previous array value
+        array[i] = array[i-1]
+
+        # the current array value can differ from the previous array value by at most 1
+        if ExtendedGenome[i-1] == symbol:
+            array[i] = array[i]-1
+        if ExtendedGenome[i+(n//2)-1] == symbol:
+            array[i] = array[i]+1
+    return array
+
+# Input:  Strings Text and Pattern
+# Output: The number of times Pattern appears in Text
+# HINT:   This code should be identical to when you last implemented PatternCount
+def PatternCount(Pattern, Text):
+    count = 0
+    for i in range(len(Text)-len(Pattern)+1):
+        if Text[i:i+len(Pattern)] == Pattern:
+            count = count+1
+    return count 
+
+
+### DO NOT MODIFY THE CODE BELOW THIS LINE ###
+import sys
+lines = sys.stdin.read().splitlines()
+print(FasterSymbolArray(lines[0],lines[1]))
