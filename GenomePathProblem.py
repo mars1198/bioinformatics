acfb0bf51a91d0f187d01fe95ff649144a352603
mars1198 <emperie2006@yandex.ru>
2018-09-27 00:28:06 +0400

    Create GenomePathProblem.py

---
+++ GenomePathProblem.py
@@ -0,0 +1,11 @@
+fin = open('dataset_198_3.txt', 'r')
+fout=open('output.txt', 'w')
+reads=[]
+for line in fin.readlines():
+    reads.append(line.replace('\n',''))
+k=len(reads[0])
+fout.write(reads[0])
+for r in reads[1:]:
+    fout.write(r[k-1:])
+fin.close()
+fout.close()
