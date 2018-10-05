d4f2685991ef22904ce484ec14fa70f9340f40ca
mars1198 <emperie2006@yandex.ru>
2018-10-06 03:10:13 +0400

    Create find_changes.py

---
+++ find_changes.py
@@ -0,0 +1,19 @@
+def find_changes(n, coins):
+    if n < 0:
+        return []
+    if n == 0:
+         return [[]]
+    all_changes = []
+    
+
+    for last_used_coin in coins:
+        combos = find_changes(n - last_used_coin, coins)
+        for combo in combos:
+            combo.append(last_used_coin)
+            all_changes.append(combo)
+            
+            
+
+    return all_changes
+
+print (len(find_changes(20, [2,3])))
